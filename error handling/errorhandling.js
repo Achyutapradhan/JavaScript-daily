a9126e82307cfb9597b3f9af22c816cc7e124a9b
Achyuta Pradhan <achyutapradhan33@gmail.com>
2026-06-23 15:19:31 +0530

    Learnt try, catch and finally

--- error handling/errorhandling.js
+++ error handling/errorhandling.js
@@ -1,8 +1,30 @@
-try
-{
-const result = somefunction();
+//simple try catch
+// try
+// {
+// const result = somefunction();
+// }
+// catch (error) {
+//     console.error(error);
+// }
+// console.log("Error handling module loaded successfully.");
+
+
+// custom error handling
+function divide(a, b) {
+    if (b === 0) {
+        throw new Error("Division by zero is not allowed.");
+    }
+    return a / b;
 }
+
+try {
+const result = divide(10, 0);
+console.log("Result:", result);
+} 
 catch (error) {
-    console.error(error);
+    console.error("Error:", error.message);
+}
+finally {
+    console.log("I'm in finally block, I will always execute.");
 }
 console.log("Error handling module loaded successfully.");
